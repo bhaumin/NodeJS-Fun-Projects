e4df5ad28e90ca5ab9d3abb92039344a6f176227
Bhaumin Shah <bhaumin@gmail.com>
2019-05-31 00:44:03 +0530

    Refactor data access code to work with the updated version of MongoDB native driver

--- library-app/server.js
+++ library-app/server.js
@@ -1,21 +1,26 @@
-var express = require('express');
-var mongodb = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectId;
-var session = require('express-session');
-var connStr = 'mongodb://localhost:27017/libraryApp';
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectId;
+const session = require('express-session');
+const connStr = 'mongodb://localhost:27017';
+const dbName = 'libraryApp';
 
-mongodb.connect(connStr, function(err, db) {
-  var collection = db.collection('navs');
+const client = new MongoClient(connStr, { useNewUrlParser: true });
+
+client.connect(function(err, client) {
+  const db = client.db(dbName);
+  const collection = db.collection('navs');
+
   collection.find({}).toArray(function(err, results) {
     initRoutes(results);
-    db.close();
   });
 });
 
+
 function initRoutes(navs) {
-  var bookRouter = require('./src/routes/bookRoutes')(mongodb, connStr, objectId, navs);
-  var adminRouter = require('./src/routes/adminRoutes')(mongodb, connStr);
-  var authRouter = require('./src/routes/authRoutes')(mongodb, connStr);
+  const bookRouter = require('./src/routes/bookRoutes')(client, dbName, objectId, navs);
+  const adminRouter = require('./src/routes/adminRoutes')(client, dbName);
+  const authRouter = require('./src/routes/authRoutes')(client, dbName);
 
   app.use('/books', bookRouter);
   app.use('/admin', adminRouter);
@@ -31,8 +36,8 @@ function initRoutes(navs) {
   });
 }
 
-var app = express();
-var port = 3000;
+const app = express();
+const port = 3000;
 
 app.use(express.static('public/dist'));
 // app.use(express.static('src/views'));
@@ -40,10 +45,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(session({secret: 'library', resave: false, saveUninitialized: false}));
 
-require('./src/config/passport')(app, mongodb, connStr);
+require('./src/config/passport')(app, client, dbName);
 
 app.set('views', './src/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.listen(port, function() {
   console.log('Listening on port ' + port);
